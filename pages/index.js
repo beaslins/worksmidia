882567b3ecd27c4fdd39a92d6e2d95fc7dc97218
Beatriz Lins <beatriz@worksmidia.com.br>
2020-12-18 15:24:37 -0300

    updated home

--- pages/index.js
+++ pages/index.js
@@ -9,36 +9,130 @@ import { faCoffee, faLightbulb } from '@fortawesome/free-solid-svg-icons'
 function Home() {
     return (
         <Layout>
-            <section id="banner">
-                <div className="container flex">
-                    <div className="boxTitle">
+            <section className="home-banner flex">
+                <div className="content">
+                    <div className="box-title">
                         <h1 className="title">Conectando <span className="yellow">pessoas</span> <span className="block">através da <span className="yellow">tecnologia!</span></span></h1>
                         <Link href="#contato">
                             <a className="btn btn-blue">Saiba mais</a>
                         </Link>
                     </div>
-                    <div>
-                        <motion.img src="/images/tophome.png" animate={{ x: 100 }} transition={{ type: "spring", fade: 0.5}}/>
+                </div>
+                <div className="image">
+                    <motion.img src="/images/tophome.png" animate={{ z: 100 }} transition={{ type: "spring", fade: 0.5}}/>
+                </div>
+            </section>
+
+            <section className="home-whyus p-box">
+                <div className="container flex">
+                    <div className="item flex">
+                        <div className="base base-one flex flex-centered">
+                            <FontAwesomeIcon icon={faLightbulb} className="icon" />
+                        </div>
+                        <h3 className="title">Meet Friends</h3>
+                        <p>Why money's in that office, right? If she start giving means some bullet about ain't there</p>
+                    </div>
+                    <div className="item flex">
+                        <div className="base base-two flex flex-centered">
+                            <FontAwesomeIcon icon={faLightbulb} className="icon" />
+                        </div>
+                        <h3 className="title">Private Community</h3>
+                        <p>Why money's in that office, right? If she start giving means some bullet about ain't there</p>
+                    </div>
+                    <div className="item flex">
+                        <div className="base base-three flex flex-centered">
+                            <FontAwesomeIcon icon={faLightbulb} className="icon" />
+                        </div>
+                        <h3 className="title">Free Membership</h3>
+                        <p>Why money's in that office, right? If she start giving means some bullet about ain't there</p>
                     </div>
                 </div>
             </section>
 
-            <section id="whyUs">
-                <div className="container flex">
-                    <div className="item">
-                        <FontAwesomeIcon icon={faLightbulb} className="icon" />
-                        <h3>Lorem Ipsum</h3>
-                        <p>Why money's in that office, right? If she start giving means some bullet about ain't there</p>
+            <section className="home-services flex">
+                <div className="content">
+                    <div className="box">
+                        <div className="box-title">
+                            <h4 className="sub-title">Serviços</h4>
+                            <h2 className="title">Easy management for your business</h2>
+                            <p>Why money's in that office, right? If she start giving means some bullet about ain't there, and we got to go placing else and get it.</p>
+                        </div>
+
+                        <div className="services-list">
+                            <div className="item flex">
+                                <div><FontAwesomeIcon icon={faLightbulb} className="icon" /></div>
+                                <div>
+                                    <h3 className="title">Easy to Manage</h3>
+                                    <p>Why money's in that office, right? If she start here giving for means some bullet about ain't there.</p>
+                                </div>
+                            </div>
+                            <div className="item flex">
+                                <div><FontAwesomeIcon icon={faLightbulb} className="icon" /></div>
+                                <div>
+                                    <h3 className="title">Easy to Manage</h3>
+                                    <p>Why money's in that office, right? If she start here giving for means some bullet about ain't there.</p>
+                                </div>
+                            </div>
+                            <div className="item flex">
+                                <div><FontAwesomeIcon icon={faLightbulb} className="icon" /></div>
+                                <div>
+                                    <h3 className="title">Easy to Manage</h3>
+                                    <p>Why money's in that office, right? If she start here giving for means some bullet about ain't there.</p>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <div className="item">
-                        <FontAwesomeIcon icon={faLightbulb} className="icon" />
-                        <h3>Lorem Ipsum</h3>
-                        <p>Why money's in that office, right? If she start giving means some bullet about ain't there</p>
+                </div>
+                <div className="image">
+                    <motion.img src="/images/services.png" animate={{ z: 100 }} transition={{ type: "spring", fade: 0.5}}/>
+                </div>
+            </section>
+
+            <section className="clients container"></section>
+
+            <section className="home-portfolio p-box">
+                <div className="port-title flex">
+                    <div className="box-title">
+                        <h4 className="sub-title">Portfólio</h4>
+                        <h2 className="title">Business has only two functions are marketing and innovation</h2>
                     </div>
-                    <div className="item">
-                        <FontAwesomeIcon icon={faLightbulb} className="icon" />
-                        <h3>Lorem Ipsum</h3>
-                        <p>Why money's in that office, right? If she start giving means some bullet about ain't there</p>
+                </div>
+                <div className="works grid">
+                    <div>
+                        <Image 
+                            src="/images/port1.jpg"
+                            alt="Cliente 1"
+                            layout="responsive"
+                            width="570"
+                            height="420"
+                        />
+                    </div>
+                    <div>
+                        <Image 
+                            src="/images/port2.jpg"
+                            alt="Cliente 1"
+                            layout="responsive"
+                            width="570"
+                            height="420"
+                        />
+                    </div>
+                    <div>
+                        <Image 
+                            src="/images/port3.jpg"
+                            alt="Cliente 1"
+                            layout="responsive"
+                            width="570"
+                            height="420"
+                        />
+                    </div>
+                    <div>
+                        <Image 
+                            src="/images/port4.jpg"
+                            alt="Cliente 1"
+                            layout="responsive"
+                            width="570"
+                            height="420"
+                        />
                     </div>
                 </div>
             </section>
